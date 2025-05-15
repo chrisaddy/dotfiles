87bcb5af49f4c177eeb5212963527438cc977591
chrisaddy <chris.william.addy@gmail.com>
2025-05-14 20:43:36 -0400

    updates

--- nix/terminal.nix
+++ nix/terminal.nix
@@ -3,20 +3,18 @@
     cmake
     jetbrains-mono
   ];
-  programs.ghostty = {
-    enable = true;
-  };
-# font-size = 24
-# theme = tokyonight-storm
-# cursor-style = block
-# shell-integration = zsh
-# shell-integration-features = cursor,no-title
-# background-opacity = 0.9
-# macos-titlebar-style = transparent
-# 
-# window-position-x = 5
-# window-position-y = 5
-# 
-# window-width = 85
-# window-height = 24
+  home.file.".config/ghostty/config".text = ''
+    font-size = 24
+    theme = tokyonight-storm
+    cursor-style = block
+    shell-integration = zsh
+    shell-integration-features = cursor,no-title
+    background-opacity = 0.9
+    macos-titlebar-style = transparent
+
+    window-position-x = 5
+    window-position-y = 5
+    window-width = 85
+    window-height = 24
+  '';
 }
