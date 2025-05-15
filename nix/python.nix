d032684139a23e3365fc38fc71f6929b7f65f9e2
chrisaddy <chris.william.addy@gmail.com>
2025-05-14 22:27:09 -0400

    updates

--- nix/python.nix
+++ nix/python.nix
@@ -8,18 +8,4 @@
     python313Packages.jedi-language-server
     pyright
   ];
-
-  home.shellAliases = {
-    "py" = "ipython";
-
-    "uvs" = "uv pip sync";
-    "uvr" = "uv run";
-
-    "rf" = "ruff format";
-    "rl" = "ruff check";
-    "rfix" = "ruff check --fix";
-
-    "venv" = "python -m venv .venv";
-    "va" = "source .venv/bin/activate";
-  };
 }
