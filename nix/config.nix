0d8f443d987923486dcb21cedb194d7e7e761065
chrisaddy <chris.william.addy@gmail.com>
2025-05-15 09:43:07 -0400

    updates

--- nix/config.nix
+++ nix/config.nix
@@ -1,15 +1,11 @@
-{
-  lib
-}: 
-let
-  username = 
-    if builtins.getEnv "USER" != "" 
-      then builtins.getEnv "USER"
-      else "chrisaddy";
+{lib}: let
+  username =
+    if builtins.getEnv "USER" != ""
+    then builtins.getEnv "USER"
+    else "caddy";
 
   homeDirectory = "/Users/${username}";
   email = "chris.william.addy@gmail.com";
-in
-{
+in {
   inherit username homeDirectory email;
 }
