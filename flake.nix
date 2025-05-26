b1d1545956a6c7cf86d99a75e3e0c74ac609ea9b
chrisaddy <chris.william.addy@gmail.com>
2025-05-25 21:40:39 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
-  description = "Chris's multi-host flake (macOS + NixOS)";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs";
 
     nix-darwin = {
       url = "github:lnl7/nix-darwin/master";
@@ -14,9 +12,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # nixvim.url = "github:nix-community/nixvim";
+    # mac-app-util.url = "github:hraban/mac-app-util";
     themes.url = "github:RGBCube/ThemeNix";
-
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
@@ -24,85 +22,88 @@
     nixpkgs,
     nix-darwin,
     home-manager,
-    flake-utils,
-    themes,
+    # nixvim,
+    # mac-app-util,
     ...
-  } @ inputs: let
+  }: let
     lib = nixpkgs.lib;
-
-    overlays = [
-      (final: prev: {
-        # Example: disable native-comp for Emacs
-        # emacsNoNativeComp = prev.emacs.override { withNativeCompilation = false; };
-      })
-    ];
-
-    mkPkgs = system:
-      import nixpkgs {
-        inherit system overlays;
-      };
+    system = "aarch64-darwin";
 
     userConfig = import ./config.nix {inherit lib;};
 
-    nixCommon = {
-      nixpkgs = {
-        overlays = overlays;
-        config = {
-          allowUnfree = true;
-          allowBroken = true;
-          allowUnsupportedSystem = true;
-          allowUnfreePredicate = pkg:
-            builtins.elem (lib.getName pkg) [];
+    overlays = [
+      (final: prev: {
+        # emacsNoNativeComp = prev.emacs.override {
+        #   withNativeCompilation = false;
+        # };
+      })
+    ];
+
+    pkgs = import nixpkgs {
+      inherit system overlays;
+    };
+
+    baseModules = [
+      {
+        nix.enable = false;
+
+        nixpkgs = {
+          hostPlatform = system;
+          overlays = overlays;
+          config.allowBroken = true;
+          config.allowUnsupportedSystem = true;
+          config.allowUnfreePredicate = pkg:
+            builtins.elem (lib.getName pkg) [
+              # "claude-code"
+              # "vscode-extension-visualjj-visualjj"
+              # "vscode"
+              # "vscode-extension-ms-vscode-remote-remote-containers"
+              # "windsurf"
+            ];
         };
+
+        nix.settings = {
+          experimental-features = "nix-command flakes pipe-operators";
+        };
+
+        users.users.${userConfig.username} = {
+          home = userConfig.homeDirectory;
+          shell = pkgs.nushell;
+        };
+
+        system.stateVersion = 6;
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+      }
+
+      home-manager.darwinModules.home-manager
+
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+
+        home-manager.users.${userConfig.username} = {
+          imports = [
+            ./home.nix
+            # nixvim.homeManagerModules.nixvim
+          ];
+        };
+
+        # home-manager.sharedModules = [
+        #   mac-app-util.homeManagerModules.default
+        # ];
+      }
+    ];
+
+    mkHost = name: {
+      darwinConfigurations.${name} = nix-darwin.lib.darwinSystem {
+        inherit system;
+        modules = baseModules;
       };
-
-      nix.settings.experimental-features = "nix-command flakes";
-    };
-  in {
-    darwinConfigurations."Chriss-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [
-        nixCommon
-
-        {
-          users.users.${userConfig.username} = {
-            home = userConfig.homeDirectory;
-            shell = (mkPkgs "aarch64-darwin").nushell;
-          };
-
-          system.stateVersion = 6;
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-        }
-
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${userConfig.username} = {
-            imports = [./home.nix];
-          };
-        }
-      ];
     };
 
-    nixosConfigurations.t470s = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        nixCommon
-        ./configuration.nix
-        ./hardware-configuration.nix
-
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${userConfig.username} = {
-            imports = [./home.nix];
-          };
-        }
-      ];
-
-      specialArgs = {inherit userConfig overlays;};
-    };
-  };
+    hosts = [
+      "Chriss-MacBook-Air"
+    ];
+  in
+    lib.foldl' (acc: name: acc // (mkHost name)) {} hosts;
 }
