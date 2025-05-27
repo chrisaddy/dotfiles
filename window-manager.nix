fda5c5ff36965bb454a5983ca5d859a7daade725
chrisaddy <chris.william.addy@gmail.com>
2025-05-27 14:10:16 -0400

    updates

--- window-manager.nix
+++ window-manager.nix
@@ -1,18 +1,21 @@
 {pkgs, ...}: {
-  programs.yabai = {
-    enable = true;
-    package = pkgs.yabai;
-    enableScriptingAddition = true;
-    config = {
-      layout = "bsp";
-      auto_balance = "on";
-      focus_follows_mouse = "autoraise";
-      mouse_follows_focus = "off";
-      window_gap = 10;
-      top_padding = 10;
-      bottom_padding = 10;
-      left_padding = 10;
-      right_padding = 10;
-    };
-  };
+  home.packages = with pkgs; [
+    yabai
+  ];
+  # programs.yabai = {
+  #   enable = true;
+  #   package = pkgs.yabai;
+  #   enableScriptingAddition = true;
+  #   config = {
+  #     layout = "bsp";
+  #     auto_balance = "on";
+  #     focus_follows_mouse = "autoraise";
+  #     mouse_follows_focus = "off";
+  #     window_gap = 10;
+  #     top_padding = 10;
+  #     bottom_padding = 10;
+  #     left_padding = 10;
+  #     right_padding = 10;
+  #   };
+  # };
 }
