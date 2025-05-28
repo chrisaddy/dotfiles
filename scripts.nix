46069f63797f7cebf8255eaf01f7849a1d913eb4
chrisaddy <chris.william.addy@gmail.com>
2025-05-27 21:19:08 -0400

    updates

--- scripts.nix
+++ scripts.nix
@@ -7,7 +7,7 @@
   home.packages = with pkgs; [
     (pkgs.writeShellScriptBin "book" ''
       #!/${pkgs.nushell}/bin/nu
-      main [url: string] {
+      def main [url: string] {
         http get $url
       }
     '')
