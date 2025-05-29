9320569b8757d7a51dbe4460455b130f531bd304
chrisaddy <chris.william.addy@gmail.com>
2025-05-29 02:10:25 -0400

    updates

--- home/common.nix
+++ home/common.nix
@@ -2,17 +2,25 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  gitUsername = "chrisaddy";
+  gitEmail = "chris.william.addy@gmail.com";
+in {
   home.stateVersion = "25.05";
 
   home.packages = with pkgs; [
     nh
+    alejandra
   ];
 
   programs.git = {
-    enable = true;
-    userName = "chrisaddy";
-    userEmail = "chris.william.addy@gmail.com";
+    userName = gitUsername;
+    userEmail = gitEmail;
+  };
+
+  programs.jujutsu.user = {
+    email = gitEmail;
+    name = gitUsername;
   };
 
   nix = {
---
+++ modules/git.nix
@@ -0,0 +1,51 @@
+{
+  pkgs,
+  lib ? null,
+  ...
+}: let
+  userConfig = import ./config.nix {inherit lib;};
+in {
+  home.packages = with pkgs; [
+    lazygit
+    lazyjj
+    jujutsu
+    radicle-node
+  ];
+  programs = {
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          email = userConfig.email;
+          name = userConfig.username;
+        };
+        ui = {
+          diff-editor = ["nvim" "-c" "DiffEditor $left $right $output"];
+        };
+      };
+    };
+    git = {
+      enable = true;
+      difftastic.enable = true;
+      ignores = [
+        "*~"
+        "*.swp"
+      ];
+      lfs.enable = true;
+      maintenance = {
+        enable = true;
+        repositories = [
+          "$HOME/projects/pocketsizefund/pocketsizefund"
+        ];
+      };
+    };
+    gh = {
+      enable = true;
+      settings = {
+        git_protocol = "ssh";
+
+        prompt = "enabled";
+      };
+    };
+  };
+}
