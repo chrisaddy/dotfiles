d5ea430e21aa21bb2a6d9579ae7ccbed79011e36
chrisaddy <chris.william.addy@gmail.com>
2025-05-29 02:02:10 -0400

    updates

--- home/common.nix
+++ home/common.nix
@@ -3,6 +3,12 @@
   pkgs,
   ...
 }: {
+  home.stateVersion = "25.05";
+
+  home.packages = with pkgs; [
+    nh
+  ];
+
   programs.git = {
     enable = true;
     userName = "chrisaddy";
--- home/m4.nix
+++ home/m4.nix
@@ -1,4 +1,9 @@
 {...}: {
+  imports = [
+    ./common.nix
+    ../modules/helix.nix
+    ../modules/nushell.nix
+  ];
   home.username = "chrisaddy";
   home.homeDirectory = "/Users/chrisaddy";
 }
