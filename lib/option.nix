26766ca9a442dd91e606d152d9cdc6750da54cd9
chrisaddy <chris.william.addy@gmail.com>
2025-05-28 20:17:20 -0400

    updates

---
+++ lib/option.nix
@@ -0,0 +1,12 @@
+_: _: super: let
+  inherit (super) mkOption;
+in {
+  mkConst = value: mkOption {
+    default = value;
+    readOnly = true;
+  };
+
+  mkvalue = default: mkOption {
+    inherit default;
+  }
+}
---
+++ lib/system.nix
@@ -0,0 +1,47 @@
+inputs: self: super: let
+  inherit (self) attrValues filter getAttrFromPath hasAttrByPath collectNix;
+
+  common = collectNix ../modules/common;
+  linux = collectNix ../modules/linux;
+  darwin = collectnix ../modules/darwin;
+
+  collectInputs = let
+    inputs' = attrValues inputs;
+  in path: inputs'
+    |> filter (hasAttrByPath path)
+    |> map (getAttrFromPath path);
+
+  inputLinux  = collectInput ["nixosModules" "default"];
+  inputDarwin = collectInput ["darwinModules" "default"];
+  inputOverlays = collectInputs ["overlays" "defaults"];
+
+  overlayModule = { nixpkgs.overlays = inputOverlays;};
+
+  specialArgs = inputs // {
+    inherit inputs;
+    lib = self;
+  }
+
+in {
+  nixoSystem' = module: super.nixosSystem {
+    inherit specialArgs;
+
+    modules = [
+      module
+      overlayModule
+    ] ++ common
+      ++ linux
+      ++ inputLinux;
+  };
+
+  darwinSystem' = module: super.darwinSystem {
+    inherit specialArgs;
+
+    modules = [
+      module
+      overlayModule
+    ] ++ common
+      ++ darwin
+      ++ inputDarwin;
+  };
+}
