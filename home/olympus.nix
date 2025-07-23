e02db36924fe8b0635346552e037f79bf3c6e07e
chrisaddy <chris.william.addy@gmail.com>
2025-07-23 11:47:55 -0400

    updates

---
+++ home/olympus.nix
@@ -0,0 +1,16 @@
+{...}: {
+  imports = [
+    ./common.nix
+    ../modules/ai.nix
+    ../modules/gcloud.nix
+    ../modules/ghostty.nix
+    ../modules/git.nix
+    ../modules/helix.nix
+    ../modules/media.nix
+    ../modules/nushell.nix
+    ../modules/ocaml.nix
+    ../modules/tmux.nix
+  ];
+  home.username = "chrisaddy";
+  home.homeDirectory = "/Users/chrisaddy";
+}
