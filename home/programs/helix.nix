f7489d4cee00abb01f66b65342114f032f8e66c0
Dave (AI) <chris.william.addy@gmail.com>
2026-05-20 16:33:39 -0400

    update

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -10,12 +10,12 @@
         insert = "bar";
         select = "underline";
       };
-    };
-    # languages.language = [{
-    #   name = "nix";
-    #   auto-format = true;
-    #   formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-    # }];
+    languages.language = [{
+    name = "nix";
+    auto-format = true;
+    formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    }
+    ];
     # themes = {
     #   autumn_night_transparent = {
     #     "inherits" = "autumn_night";
@@ -23,4 +23,5 @@
     #   };
     # };
   };
+};
 }
