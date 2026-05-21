88e8f82d4612e1a90d125d62b23d24a9d30ffcf1
Dave (AI) <chris.william.addy@gmail.com>
2026-05-21 00:08:28 -0400

    update

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -30,6 +30,13 @@
             l = {
               g = [":new" ":insert-output ${pkgs.lazygit}/bin/lazygit" ":buffer-close!" ":redraw"];
             };
+            o = [
+              ":sh rm -f /tmp/unique-file"
+              ":insert-output ${pkgs.yazi}/bin/yazi \"%{buffer_name}\" --chooser-file=/tmp/unique-file"
+              ":sh printf \"\x1b\[?1049h\x1b\[?2004h\" > /dev/tty"
+              ":open %sh{cat /tmp/unique-file}"
+              ":redraw'"
+            ];
             z = ":w";
           };
           Z = {
