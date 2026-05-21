749cf17212b33509371d4bf35e434d1d17bda362
Dave (AI) <chris.william.addy@gmail.com>
2026-05-20 23:49:12 -0400

    update

--- flake.nix
+++ flake.nix
@@ -11,63 +11,59 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { nixpkgs, home-manager, nix-darwin, nixvim, ... }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      mkHome = system: username: { headless ? false }: home-manager.lib.homeManagerConfiguration {
+  outputs = {
+    nixpkgs,
+    home-manager,
+    nix-darwin,
+    ...
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+    mkHome = system: username: {headless ? false}:
+      home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
         modules = [
-          nixvim.homeModules.nixvim
           ./home
         ];
         extraSpecialArgs = {
           inherit username headless;
         };
       };
-    in
-    {
-      darwinConfigurations."olympus-3" = nix-darwin.lib.darwinSystem {
-        modules = [
-          ./darwin
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.chrisaddy = import ./home;
-            home-manager.extraSpecialArgs = {
-              username = "chrisaddy";
-              headless = false;
-            };
-            home-manager.sharedModules = [
-              nixvim.homeModules.nixvim
-            ];
-          }
-        ];
-      };
-
-      homeConfigurations = let
-        darwinHome = mkHome "aarch64-darwin" "chrisaddy" {};
-        linuxHome = mkHome "x86_64-linux" "chrisaddy" {};
-      in {
-        # nh auto-detection (user@hostname)
-        "chrisaddy@olympus-3" = darwinHome;
-        "chrisaddy" = darwinHome;
-        # Explicit platform targets
-        "chrisaddy@darwin" = darwinHome;
-        "chrisaddy@darwin-x86" = mkHome "x86_64-darwin" "chrisaddy" {};
-        "chrisaddy@linux" = linuxHome;
-        "chrisaddy@linux-arm" = mkHome "aarch64-linux" "chrisaddy" {};
-        # exe.dev VM (lightweight, headless)
-        "exedev@linux" = mkHome "x86_64-linux" "exedev" { headless = true; };
-      };
+  in {
+    darwinConfigurations."olympus-3" = nix-darwin.lib.darwinSystem {
+      modules = [
+        ./darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.chrisaddy = import ./home;
+          home-manager.extraSpecialArgs = {
+            username = "chrisaddy";
+            headless = false;
+          };
+        }
+      ];
     };
+
+    homeConfigurations = let
+      darwinHome = mkHome "aarch64-darwin" "chrisaddy" {};
+      linuxHome = mkHome "x86_64-linux" "chrisaddy" {};
+    in {
+      # nh auto-detection (user@hostname)
+      "chrisaddy@olympus-3" = darwinHome;
+      "chrisaddy" = darwinHome;
+      # Explicit platform targets
+      "chrisaddy@darwin" = darwinHome;
+      "chrisaddy@darwin-x86" = mkHome "x86_64-darwin" "chrisaddy" {};
+      "chrisaddy@linux" = linuxHome;
+      "chrisaddy@linux-arm" = mkHome "aarch64-linux" "chrisaddy" {};
+      # exe.dev VM (lightweight, headless)
+      "exedev@linux" = mkHome "x86_64-linux" "exedev" {headless = true;};
+    };
+  };
 }
--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -9,6 +9,7 @@
     shellAliases = {
       vim = "nvim";
       ls = "exa -la";
+      lss = "yazi";
       ccc = "claude --dangerously-skip-permissions";
       update = "pushd $HOME/dotfiles && git pull && nh home switch . -c $(whoami)@$(if [ $(uname) = Darwin ]; then echo darwin; else echo linux; fi) && nh clean all && git add -A && git commit -m 'update' && git push; popd";
     };
