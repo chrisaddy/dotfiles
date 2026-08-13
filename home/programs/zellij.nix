dfbf3cd85dc36e8209de22e2c296fd942594c194
Dave (AI) <chris.william.addy@gmail.com>
2026-08-13 11:03:31 -0400

    update

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -7,6 +7,45 @@
     settings = {
       # keybinds._props.clear-defaults = true;
       keybinds = {
+        tab._children = [
+          {
+            bind = {
+              _args = [ "h" ];
+              _children = [
+                { MoveFocus = "Left"; }
+                { SwitchToMode._args = [ "normal" ]; }
+              ];
+            };
+          }
+          {
+            bind = {
+              _args = [ "j" ];
+              _children = [
+                { MoveFocus = "Down"; }
+                { SwitchToMode._args = [ "normal" ]; }
+              ];
+            };
+          }
+          {
+            bind = {
+              _args = [ "k" ];
+              _children = [
+                { MoveFocus = "Up"; }
+                { SwitchToMode._args = [ "normal" ]; }
+              ];
+            };
+          }
+          {
+            bind = {
+              _args = [ "l" ];
+              _children = [
+                { MoveFocus = "Right"; }
+                { SwitchToMode._args = [ "normal" ]; }
+              ];
+            };
+          }
+
+        ];
         pane._children = [
           {
             bind = {
